08b953900121204480aae8287e7eb8f2125c9f78
okoli-ryan <okoliryan50@gmail.com>
2023-08-21 19:30:54 +0100

    setup for providus transfer to main app flow

--- src/navigation/PublicNavigator.js
+++ src/navigation/PublicNavigator.js
@@ -13,7 +13,7 @@ const Stack = createStackNavigator();
 
 export default function PublicNavigator() {
   const {
-    context: { initialAuthScreen = 'Auth' },
+    context: { initialAuthScreen = 'Send' },
   } = useRehiveContext();
   if (!initialAuthScreen) {
     return <SplashScreen />;
